cacc5c762c74a49cd24074da3dfee36268ffdb6c
loic.calvez <loicca95@msn.com>
2015-03-01 15:37:37 +0100

    Ajout des classes Etudiant.cs et Promotion.cs

---
+++ GestionPromotion/GestionPromotion.Entity.BC/Etudiant.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPromotion.Entity.BC
+{
+    class Etudiant
+    {
+        // Variables de la classe Etudiant
+        private String m_prenom;
+        private String m_nom;
+        private String m_date_de_naissance;
+
+        // Getter/Setter
+        public String Prenom
+        {
+            get { return m_prenom; }
+            set { m_prenom = value; }
+        }
+        public String Nom
+        {
+            get { return m_nom; }
+            set { m_nom = value; }
+        }
+        public String Date_de_naissance
+        {
+            get { return m_date_de_naissance; }
+            set { m_date_de_naissance = value; }
+        }
+
+        // Constructeur : tous les paramètres doivent être passés.
+        public Etudiant(String p_prenom, String p_nom, String p_date_de_naissance)
+        {
+            if((p_prenom != null) && (p_nom != null) && (p_date_de_naissance != null))
+            {
+                this.m_prenom = p_prenom;
+                this.m_nom = p_nom;
+                this.m_date_de_naissance = p_date_de_naissance;
+            }
+        }
+
+        // Méthodes de la classe Etudiant
+        // ICI
+    }
+}
---
+++ GestionPromotion/GestionPromotion.Entity.BC/Promotion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPromotion.Entity.BC
+{
+    class Promotion
+    {
+        // Variables de la classe Promotion
+        private String m_nom;
+        private Int16 m_annee;
+        private List<Etudiant> m_liste_etudiant;
+
+        // Getter/Setter
+        public String Nom
+        {
+            get { return m_nom; }
+            set { m_nom = value; }
+        }
+        public Int16 Annee
+        {
+            get { return m_annee; }
+            set { m_annee = value; }
+        }
+        internal List<Etudiant> Liste_etudiant
+        {
+            get { return m_liste_etudiant; }
+            set { m_liste_etudiant = value; }
+        }
+
+        // Constructeurs
+        // Constructeur 1 : tous les paramètres sont passés
+        public Promotion(String p_nom, Int16 p_annee, List<Etudiant> p_liste_etudiant)
+        {
+            if ((p_nom != null) && (m_annee != null))
+            {
+                this.m_nom = p_nom;
+                this.m_annee = p_annee;
+            }
+            this.m_liste_etudiant = p_liste_etudiant;
+        }
+
+        // Constructeur 2 : il n'y a pas encore d'étudiant dan la liste
+        public Promotion(String p_nom, Int16 p_annee)
+        {
+            if ((p_nom != null) && (m_annee != null))
+            {
+                this.m_nom = p_nom;
+                this.m_annee = p_annee;
+            }
+            this.m_liste_etudiant = null;
+        }
+
+        // Méthodes de la classe Etudiant
+        // ICI
+    }
+}
