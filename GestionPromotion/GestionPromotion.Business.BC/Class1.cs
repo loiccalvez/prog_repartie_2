c6aeeef41b3e02163771da5620e4e1ee80121fb8
dydy <xv-2046@hotmail.fr>
2015-03-01 15:02:28 +0100

    Ajout de la solution

---
+++ GestionPromotion/GestionPromotion.Business.BC/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPromotion.Business.BC
+{
+    public class Class1
+    {
+    }
+}
